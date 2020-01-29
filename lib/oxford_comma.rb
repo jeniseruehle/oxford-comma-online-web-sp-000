bb5261be4fc3afe797b1c41cc3cf0603a9388a91
Jenise Ruehle <jela07@gmail.com>
2020-01-29 19:06:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
       array.join(" and ")
   elsif array.length == 3
       array << "and"
-      array.join(",")
+      array.join(" , ")
   elsif array.length > 3
       array.join
   end
