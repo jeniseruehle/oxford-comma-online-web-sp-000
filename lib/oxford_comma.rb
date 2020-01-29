349ee8efefd0b3785eaa58b01e214f0776e20810
Jenise Ruehle <jela07@gmail.com>
2020-01-29 19:04:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
   case
-  when array.length == 1 
-     array.join
-  when array.length == 2 
-    array << "and"
-    array.join(",")
+    when array.length == 1 
+      array.join
+    when array.length == 2 
+      array.join("and")
+    when array.length == 3
+      array << "and"
+      array.join(",")
   end
 end
